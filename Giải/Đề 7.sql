25a644b32ab78e0592c0e1823dfdc6a084c1ecc1
Only Huân <huannd0101@gmail.com>
2021-05-19 08:10:04 +0700

    ...

---
+++ Giải/Đề 7.sql
@@ -0,0 +1,131 @@
+﻿--Ð? 7
+USE master
+GO
+DROP DATABASE QLSinhVien
+GO
+
+CREATE DATABASE QLSinhVien
+GO
+
+USE QLSinhVien
+GO
+
+CREATE TABLE Khoa (
+	MaKhoa CHAR(5) PRIMARY KEY, 
+	TenKhoa NVARCHAR(30), 
+	SoDienThoai CHAR(20)
+)
+
+CREATE TABLE Lop (
+	MaLop CHAR(5) PRIMARY KEY, 
+	TenLop NVARCHAR(30), 
+	SiSo INT, 
+	MaKhoa CHAR(5) FOREIGN KEY(MaKhoa) REFERENCES Khoa(MaKhoa)
+)
+
+CREATE TABLE SinhVien (
+	MaSV CHAR(5) PRIMARY KEY, 
+	HoTen NVARCHAR(30), 
+	GioiTinh NVARCHAR(20), 
+	NgaySinh DATE, 
+	MaLop CHAR(5) FOREIGN KEY(MaLop) REFERENCES Lop(MaLop)
+)
+
+INSERT INTO Khoa VALUES
+('K01', N'Khoa 1', '0375417807'),
+('K02', N'Khoa 2', '0375417808'),
+('K03', N'Khoa 3', '0375417809')
+
+INSERT INTO Lop VALUES 
+('L01', N'A1', 44, 'K01'),
+('L02', N'A2', 65, 'K02'),
+('L03', N'A3', 67, 'K02')
+
+INSERT INTO Sinhvien VALUES 
+('SV01', N'Nguyen Thi Ty', N'Nu', '2000/03/13', 'L01'),
+('SV02', N'Hoang Van Thai', N'Nam', '1999/03/13', 'L01'),
+('SV03', N'Nguyen Thi Ngoc', N'Nu', '2001/05/13', 'L01'),
+('SV04', N'Hoang Thi Nam', N'Nu', '2000/03/13', 'L02'),
+('SV05', N'Le Anh Tuyet', N'Nu', '1997/05/25', 'L03')
+
+SELECT * FROM Khoa
+SELECT * FROM Lop
+SELECT * FROM SinhVien
+
+--Câu 2:tạo hàm
+GO
+CREATE FUNCTION fn_cau2 (@tenKhoa NVARCHAR(30))
+RETURNS @table TABLE(MaLop CHAR(5), TenLop NVARCHAR(30), SiSo INT)
+AS
+BEGIN
+	INSERT INTO @table
+	SELECT MaLop, TenLop, SiSo
+	FROM Khoa INNER JOIN Lop
+	ON Khoa.MaKhoa = Lop.MaKhoa
+	WHERE TenKhoa = @tenKhoa
+
+	RETURN
+END
+
+--Thực thi
+GO
+SELECT * FROM dbo.fn_cau2 (N'Khoa 1')
+
+--CÂU 3
+GO
+CREATE PROC p_cau3 (@maSV CHAR(5), @hoTen NVARCHAR(30), @ngaySinh DATE, @gioiTinh NVARCHAR(20), @tenLOP NVARCHAR(30))
+AS
+BEGIN
+	--KTRA TÊN L?P
+	IF(NOT EXISTS(SELECT * FROM Lop WHERE TenLop = @tenLOP))
+		BEGIN
+			PRINT N'Không có lớp này :v'
+			RETURN 
+		END
+
+	DECLARE @maLop CHAR(5) = (
+		SELECT MaLop
+		FROM Lop
+		WHERE TenLop = @tenLOP
+	)
+
+	INSERT INTO SinhVien VALUES (@maSV, @hoTen, @gioiTinh, @ngaySinh, @maLop)
+END
+
+--TH?C THI
+--THÊM THÀNH CÔNG
+EXEC p_cau3 'SV06', N'Huân đep trai', '2001-01-01', N'Nam', 'A1'
+SELECT * FROM SinhVien
+--THÊM TH?T B?I: TÊN L?P K CÓ
+EXEC p_cau3 'SV07', N'Huân đẹp trai', '2001-01-01', N'Nam', 'A7'
+SELECT * FROM SinhVien
+
+--Câu 4:
+CREATE TRIGGER tg_cau4 
+ON SinhVien
+FOR UPDATE
+AS
+BEGIN
+	DECLARE @siSoLiopMoi INT = (
+		SELECT SiSo
+		FROM inserted INNER JOIN Lop 
+		ON inserted.MaLop = Lop.MaLop
+	)
+	IF(@siSoLiopMoi >= 80)
+		BEGIN
+			RAISERROR(N'Lớp nhiều học sinh, không thể thêm', 16, 1)
+			ROLLBACK TRAN
+		END
+	ELSE 
+		BEGIN
+			UPDATE Lop
+			SET SiSo = SiSo + 1
+			WHERE MaLop = (SELECT MaLop FROM inserted)
+
+			UPDATE Lop
+			SET SiSo = SiSo - 1
+			WHERE MaLop = (SELECT MaLop FROM deleted)
+		END
+END
+
+--TH?C THI
---
+++ Giải/Đề 8.sql
@@ -0,0 +1,150 @@
+﻿--Đề 8
+USE master
+GO
+
+DROP DATABASE QLBanHang
+GO
+
+CREATE DATABASE QLBanHang
+GO
+
+USE QLBanHang
+GO
+
+CREATE TABLE CongTy (
+	MaCongTy CHAR(5) PRIMARY KEY, 
+	TenCongTY NVARCHAR(30), 
+	DiaChi NVARCHAR(30)
+)
+
+CREATE TABLE SanPham (
+	MaSanPham CHAR(5) PRIMARY KEY, 
+	TenSanPham NVARCHAR(30), 
+	SoLuongCo INT, 
+	GiaBan MONEY
+)
+
+CREATE TABLE CungUng (
+	MaCongTy CHAR(5), 
+	MaSanPham CHAR(5), 
+	SoLuongCungUng INT, 
+	NgayCungUng DATE, 
+	PRIMARY KEY(MaCongTy, MaSanPham), 
+	CONSTRAINT pk_ct FOREIGN KEY(MaCongTy) REFERENCES CongTy(MaCongTy), 
+	CONSTRAINT pk_sp FOREIGN KEY(MaSanPham) REFERENCES SanPham(MaSanPham)
+)
+
+INSERT INTO CongTy VALUES
+('CT01', N'Công ty 1', N'Hà Nội'), 
+('CT02', N'Công ty 2', N'Điện Biên'), 
+('CT03', N'Công ty 3', N'Hà Nam')
+
+INSERT INTO SanPham VALUES
+('SP01', N'Sản phẩm 1', 10, 20000),
+('SP02', N'Sản phẩm 2', 20, 30000),
+('SP03', N'Sản phẩm 3', 30, 40000) 
+
+INSERT INTO CungUng VALUES
+('CT01', 'SP01', 10, '2021-01-01'),
+('CT01', 'SP02', 20, '2021-02-01'),
+('CT01', 'SP03', 30, '2021-03-01'),
+('CT02', 'SP01', 40, '2021-04-01'),
+('CT03', 'SP01', 50, '2021-05-01')
+
+SELECT * FROM CongTy
+SELECT * FROM SanPham
+SELECT * FROM CungUng
+
+--CÂU 2: TẠO HÀM
+GO
+CREATE FUNCTION fn_cau2 (@tenCT NVARCHAR(30), @ngayCungUng DATE)
+RETURNS @table TABLE (TenSP NVARCHAR(30), SoLuong INT, GiaBan MONEY)
+AS
+BEGIN
+	INSERT INTO @table
+	SELECT TenSanPham, SoLuongCo, GiaBan
+	FROM CongTy INNER JOIN CungUng
+	ON CongTy.MaCongTy = CungUnG.MaCongTy
+	INNER JOIN SanPham
+	ON SanPham.MaSanPham = CungUng.MaSanPham
+	WHERE TenCongTY = @tenCT AND NgayCungUng = @ngayCungUng
+
+	RETURN
+END
+
+--THỰC THI
+GO
+SELECT * FROM dbo.fn_cau2 (N'Công ty 1', '2021-02-01')
+
+--Câu 3: Tạo proc
+GO
+CREATE PROC	p_cau3 (@maCT CHAR(5), @tenCT NVARCHAR(30), @diaChi NVARCHAR(30), @kq INT OUTPUT)
+AS
+BEGIN
+	IF EXISTS(SELECT * FROM CongTy WHERE TenCongTY = @tenCT)
+		BEGIN
+			SET @kq = 1
+			PRINT N'Tên công ty đã tồn tại'
+			RETURN
+		END
+	--THÊM
+	INSERT INTO CongTy VALUES (@maCT, @tenCT, @diaChi)
+	SET @kq = 0
+END
+
+--Thực thi
+--không thành công
+GO
+DECLARE @kq INT
+EXEC p_cau3 'CT04', N'Công ty 1', N'Hải Dương', @kq OUTPUT
+SELECT @kq AS N'Kết quả'
+SELECT * FROM CongTy 
+
+--thành công
+GO
+DECLARE @kq INT
+EXEC p_cau3 'CT04', N'Công ty 4', N'Hải Dương', @kq OUTPUT
+SELECT @kq AS N'Kết quả'
+SELECT * FROM CongTy 
+
+--Câu 4: tạo trigger
+GO
+CREATE TRIGGER tg_cau4 
+ON CungUng 
+FOR UPDATE
+AS
+BEGIN
+	DECLARE @soLuongCungUngCu INT = (SELECT SoLuongCungUng FROM deleted)
+	DECLARE @soLuongCungUngMoi INT = (SELECT SoLuongCungUng FROM inserted)
+	DECLARE @soLuongCo INT = (
+		SELECT SoLuongCO 
+		FROM inserted INNER JOIN SanPham 
+		ON inserted.MaSanPham = SanPham.MaSanPham
+	)
+	DECLARE @soLuongChenhLech INT = @soLuongCungUngMoi - @soLuongCungUngCu
+	IF(@soLuongChenhLech <= @soLuongCo)
+		BEGIN 
+			UPDATE SanPham 
+			SET SoLuongCo = SoLuongCo - @soLuongChenhLech
+			WHERE MaSanPham = (SELECT MaSanPham FROM inserted)
+		END
+	ELSE 
+		BEGIN
+			RAISERROR(N'Không thể cập nhập vì số lượng không đủ', 16, 1)
+			ROLLBACK TRAN
+		END
+END
+
+--Thực thi
+--Không thành công
+UPDATE CungUng
+SET SoLuongCungUng = 21
+WHERE MaCongTy = 'CT01' AND MaSanPham = 'SP01'
+
+--Thành công
+UPDATE CungUng
+SET SoLuongCungUng = 20
+WHERE MaCongTy = 'CT01' AND MaSanPham = 'SP01'
+
+SELECT * FROM SanPham
+SELECT * FROM CungUng
