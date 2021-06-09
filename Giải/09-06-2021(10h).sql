8b875e176bb115db7ce0b51925b6c7c8a8747654
Only Huân <huannd0101@gmail.com>
2021-06-09 11:10:14 +0700

    update 09-06-2021

---
+++ Giải/09-06-2021(10h).sql
@@ -0,0 +1,160 @@
+﻿USE master
+GO
+
+-- tạo sơ sở dữ liệu 
+CREATE DATABASE QLThuoc 
+GO
+
+USE QLThuoc 
+GO
+--tạo bảng Thuoc
+CREATE TABLE Thuoc (
+	MaThuoc CHAR(3) PRIMARY KEY, 
+	TenThuoc NVARCHAR(30) NOT NULL, 
+	SoLuong INT NOT NULL, 
+	DonGia MONEY NOT NULL,
+	NhaSX NVARCHAR(30)  NOT NULL
+)
+--tạo bảng BenhNhan
+CREATE TABLE BenhNhan (
+	MaBN CHAR(4) PRIMARY KEY, 
+	TenBN NVARCHAR(30) NOT NULL,
+	GioiTinh NVARCHAR(15) NOT NULL
+)
+--tạo bảng DonThuoc
+CREATE TABLE DonThuoc (
+	MaDon CHAR(5), 
+	MaThuoc CHAR(3), 
+	SoLuongBan INT NOT NULL, 
+	NgayBan DATE, 
+	MaBN CHAR(4), 
+	PRIMARY KEY(MaDon, MaThuoc), 
+	CONSTRAINT fk_donThuoc_benhnhan FOREIGN KEY(MaBN) REFERENCES BenhNhan(MaBN),
+	CONSTRAINT fk_donThuoc_thuoc FOREIGN KEY(MaThuoc) REFERENCES Thuoc(MaThuoc)
+)
+GO
+
+-- thêm dữ liệu cho bảng Thuoc
+INSERT INTO Thuoc (MaThuoc, TenThuoc, SoLuong, DonGia, NhaSX)
+VALUES('T01', N'Thuốc an thần', 20, 200000, N'Nhà sản xuất A')
+INSERT INTO Thuoc (MaThuoc, TenThuoc, SoLuong, DonGia, NhaSX)
+VALUES('T02', N'Thuốc dạ dày', 30, 300000, N'Nhà sản xuất B')
+INSERT INTO Thuoc (MaThuoc, TenThuoc, SoLuong, DonGia, NhaSX)
+VALUES('T03', N'Thuốc đau đầu', 40, 400000, N'Nhà sản xuất C')
+
+-- thêm dữ liệu cho bảng BenhNhan
+INSERT INTO BenhNhan (MaBN, TenBN, GioiTinh)
+VALUES ('BN01', N'Đào Thu Phương', N'Nữ')
+INSERT INTO BenhNhan (MaBN, TenBN, GioiTinh)
+VALUES ('BN02', N'Trần Khắc Bình Dương', N'Nam')
+INSERT INTO BenhNhan (MaBN, TenBN, GioiTinh)
+VALUES ('BN03', N'Nguyễn Đình Huân', N'Nam')
+-- thêm dữ liệu cho bảng DonThuoc
+INSERT INTO DonThuoc (MaDon, MaThuoc, SoLuongBan, NgayBan, MaBN)
+VALUES ('DT01', 'T01', 4, '2021-01-01', 'BN01')
+INSERT INTO DonThuoc (MaDon, MaThuoc, SoLuongBan, NgayBan, MaBN)
+VALUES ('DT02', 'T03', 5, '2021-01-02', 'BN01')
+INSERT INTO DonThuoc (MaDon, MaThuoc, SoLuongBan, NgayBan, MaBN)
+VALUES ('DT03', 'T01', 6, '2021-01-03', 'BN02')
+INSERT INTO DonThuoc (MaDon, MaThuoc, SoLuongBan, NgayBan, MaBN)
+VALUES ('DT04', 'T02', 7, '2021-01-04', 'BN02')
+INSERT INTO DonThuoc (MaDon, MaThuoc, SoLuongBan, NgayBan, MaBN)
+VALUES ('DT05', 'T01', 8, '2021-01-05', 'BN03')
+
+--Xem dữ liệu các bảng 
+SELECT * FROM Thuoc
+SELECT * FROM BenhNhan
+SELECT * FROM DonThuoc
+
+--Câu 2:
+GO
+CREATE FUNCTION fn_cau2(@nhaSX NVARCHAR(30), @ngayBan DATE)
+RETURNS @table TABLE(MaBN CHAR(4), TenBN NVARCHAR(30), GioiTinh NVARCHAR(15), MaThuoc CHAR(3), TenThuoc NVARCHAR(30), SoLuong INT, DonGia MONEY)
+AS
+BEGIN
+	INSERT INTO @table
+	SELECT BenhNhan.MaBN, TenBN, GioiTinh, Thuoc.MaThuoc, TenThuoc, SoLuong, DonGia
+	FROM Thuoc INNER JOIN DonThuoc ON Thuoc.MaThuoc = DonThuoc.MaThuoc
+				INNER JOIN BenhNhan ON BenhNhan.MaBN = DonThuoc.MaBN
+	WHERE NgayBan = @ngayBan AND NhaSX = @nhaSX
+	RETURN
+END
+
+--Thực thi:
+SELECT * FROM dbo.fn_cau2 (N'Nhà sản xuất A', '2021-01-01')
+
+--Câu 3:
+GO
+CREATE PROC p_cau3 (@maDon CHAR(4), @tenThuoc NVARCHAR(30), @soLuongBan INT, @ngayBan DATE, @maBN CHAR(4))
+AS
+BEGIN
+	IF(NOT EXISTS(SELECT * FROM Thuoc WHERE TenThuoc = @tenThuoc))
+		BEGIN
+			PRINT N'Tên thuốc không tồn tại'
+			RETURN
+		END
+	ELSE 
+		BEGIN
+			DECLARE @maThuoc CHAR(3) = (
+				SELECT MaThuoc
+				FROM Thuoc
+				WHERE TenThuoc = @tenThuoc
+			)
+			INSERT INTO DonThuoc (MaDon, MaThuoc, SoLuongBan, NgayBan, MaBN)
+			VALUES (@maDon, @maThuoc, @soLuongBan, @ngayBan, @maBN)
+		END
+END
+
+--Thực thi
+--thêm không thành công(không có tên: Thuốc đau bụng)
+EXEC p_cau3 'DT06', N'Thuốc đau bụng', 8, '2021-01-05', 'BN03'
+SELECT * FROM DonThuoc
+--thêm thành công
+EXEC p_cau3 'DT06', N'Thuốc an thần', 8, '2021-01-05', 'BN03'
+SELECT * FROM DonThuoc
+
+GO
+--Câu 4:
+
+CREATE TRIGGER tg_cau4
+ON DonThuoc
+FOR INSERT
+AS
+BEGIN
+	DECLARE @SoLuongBan INT = (
+			SELECT SoLuongBan
+			FROM inserted
+		)
+
+	DECLARE @SoLuong INT = (
+		SELECT SoLuong
+		FROM Thuoc INNER JOIN inserted
+		ON Thuoc.MaThuoc = inserted.MaThuoc
+	)
+
+	IF(@SoLuong < @SoLuongBan)
+		BEGIN
+			RAISERROR(N'Không đủ số lượng để bán hàng', 16, 1)
+			ROLLBACK TRAN
+		END
+	ELSE
+		BEGIN
+			UPDATE Thuoc
+			SET SoLuong = SoLuong - @SoLuongBan
+			FROM Thuoc INNER JOIN inserted
+			ON Thuoc.MaThuoc = inserted.MaThuoc
+		END
+END
+GO
+--Thực thi
+--không thành công(số lượng không đủ)
+INSERT INTO DonThuoc (MaDon, MaThuoc, SoLuongBan, NgayBan, MaBN)
+VALUES ('DT08', 'T02', 10000, '2021-01-01', 'BN02')
+SELECT * FROM Thuoc
+SELECT * FROM DonThuoc
+
+--thành công(số lượng đủ)
+INSERT INTO DonThuoc (MaDon, MaThuoc, SoLuongBan, NgayBan, MaBN)
+VALUES ('DT07', 'T03', 2, '2021-01-01', 'BN01')
+SELECT * FROM Thuoc
+SELECT * FROM DonThuoc
---
+++ Giải/09-06-2021(7h45).sql
@@ -0,0 +1,133 @@
+﻿CREATE DATABASE QLSV
+GO
+
+USE QLSV
+GO
+
+CREATE TABLE Khoa (
+	MaKhoa CHAR(3) PRIMARY KEY, 
+	TenKhoa NVARCHAR(30) NOT NULL, 
+	DiaChi NVARCHAR(30) NOT NULL, 
+	SoDT CHAR(15) NOT NULL, 
+	Email CHAR(30) NOT NULL
+)
+
+GO
+CREATE TABLE Lop (
+	MaLop CHAR(3) PRIMARY KEY, 
+	TenLop NVARCHAR(30) NOT NULL,
+	SiSo INT NOT NULL,
+	MaKhoa CHAR(3), 
+	CONSTRAINT fk_lop_khoa FOREIGN KEY(MaKhoa) REFERENCES Khoa(MaKhoa)
+)
+
+GO
+CREATE TABLE SinhVien (
+	MaSV CHAR(4) PRIMARY KEY, 
+	HoTen NVARCHAR(30) NOT NULL,
+	NgaySinh DATE  NOT NULL,
+	GioiTinh NVARCHAR(15) NOT NULL,
+	MaLop CHAR(3), 
+	CONSTRAINT fk_sinhVien_lop FOREIGN KEY (MaLop) REFERENCES Lop(MaLop)
+)
+
+GO
+INSERT INTO Khoa (MaKhoa, TenKhoa, DiaChi, SoDT, Email)
+VALUES('K01', N'CNTT', N'Cơ Sở Minh Khai', '0123456789', 'khoacntt@mail.com')
+INSERT INTO Khoa (MaKhoa, TenKhoa, DiaChi, SoDT, Email)
+VALUES('K02', N'Điện Tử', N'Cơ Sở Tây Tựu', '0123456788', 'khoadientu@mail.com')
+INSERT INTO Khoa (MaKhoa, TenKhoa, DiaChi, SoDT, Email)
+VALUES('K03', N'Kế toán', N'Cơ Sở Minh Khai', '0123456787', 'khoaketoan@mail.com')
+
+GO
+INSERT INTO Lop (MaLop, TenLop, SiSo, MaKhoa)
+VALUES ('L01', N'CNTT05', 81, 'K01')
+INSERT INTO Lop (MaLop, TenLop, SiSo, MaKhoa)
+VALUES ('L02', N'DienTu01', 60, 'K02')
+INSERT INTO Lop (MaLop, TenLop, SiSo, MaKhoa)
+VALUES ('L03', N'KeToan02', 65, 'K03')
+
+GO
+INSERT INTO SinhVien (MaSV, HoTen, NgaySinh, GioiTinh, MaLop)
+VALUES ('SV01', N'Đào Thu Phương', '2000-01-01', N'Nữ', 'L01')
+INSERT INTO SinhVien (MaSV, HoTen, NgaySinh, GioiTinh, MaLop)
+VALUES ('SV02', N'Nguyễn Đinh Huân', '2001-01-01', N'Nam', 'L01')
+INSERT INTO SinhVien (MaSV, HoTen, NgaySinh, GioiTinh, MaLop)
+VALUES ('SV03', N'Nguyễn Thị A', '2002-01-01', N'Nữ', 'L02')
+INSERT INTO SinhVien (MaSV, HoTen, NgaySinh, GioiTinh, MaLop)
+VALUES ('SV04', N'Trần Văn A', '2003-01-01', N'Nam', 'L02')
+INSERT INTO SinhVien (MaSV, HoTen, NgaySinh, GioiTinh, MaLop)
+VALUES ('SV05', N'Nguyễn Thị B', '2004-01-01', N'Nữ', 'L03')
+
+--xem dữ liệu
+SELECT * FROM Khoa
+SELECT * FROM Lop
+SELECT * FROM SinhVien
+
+--Câu 2:
+GO
+CREATE FUNCTION fn_cau2 (@tenKhoa NVARCHAR(30))
+RETURNS @table TABLE(MaSV CHAR(4), HoTen NVARCHAR(30), Tuoi INT)
+AS
+BEGIN
+	INSERT INTO @table
+	SELECT MaSV, HoTen, (YEAR(GETDATE()) - YEAR(NgaySinh))
+	FROM SinhVien INNER JOIN Lop
+	ON SinhVien.MaLop = Lop.MaLop
+	INNER JOIN Khoa
+	ON Lop.MaKhoa = Khoa.MaKhoa
+	WHERE TenKhoa = @tenKhoa
+	RETURN 
+END
+--thực thi
+SELECT * FROM dbo.fn_cau2 (N'CNTT')
+
+--Câu 3:
+GO
+CREATE PROC p_cau3 (@tuTuoi INT, @denTuoi INT, @tenLop NVARCHAR(30))
+AS
+BEGIN
+	SELECT MaSV, HoTen, NgaySinh, TenLop, TenKhoa, (YEAR(GETDATE()) - YEAR(NgaySinh)) AS N'Ngày Sinh'
+	FROM Khoa INNER JOIN Lop ON Khoa.MaKhoa = Lop.MaKhoa
+				INNER JOIN SinhVien ON Lop.MaLop = SinhVien.MaLop
+	WHERE (YEAR(GETDATE()) - YEAR(NgaySinh)) BETWEEN @tuTuoi AND @denTuoi
+			AND TenLop = @tenLop
+END
+
+--Thực thi
+EXEC p_cau3 21, 21, N'CNTT05'
+
+--Câu 4
+GO
+CREATE TRIGGER tg_cau4 
+ON SinhVien
+FOR INSERT
+AS
+BEGIN
+	DECLARE @maLop CHAR(3) = (SELECT MaLop FROM inserted)
+	DECLARE @maSV CHAR(4) = (SELECT MaSV FROM inserted)
+	DECLARE @siSo INT = (SELECT SiSo FROM Lop WHERE MaLop = @maLop)
+	IF(@siSo > 80)
+		BEGIN
+			RAISERROR(N'Không thể thêm', 16, 1)
+			ROLLBACK TRAN
+		END
+	ELSE 
+		BEGIN
+			UPDATE Lop
+			SET SiSo = SiSo + 1
+			WHERE MaLop = @maLop
+		END
+END
+
+--thực thi
+--không thành công
+INSERT INTO SinhVien (MaSV, HoTen, NgaySinh, GioiTinh, MaLop)
+VALUES ('SV06', N'Nguyễn Thị Tý', '2000', N'Nữ', 'L01')
+SELECT * FROM Lop
+SELECT * FROM SinhVien
+--thành công
+INSERT INTO SinhVien (MaSV, HoTen, NgaySinh, GioiTinh, MaLop)
+VALUES ('SV06', N'Nguyễn Thị Tý', '2000', N'Nữ', 'L02')
+SELECT * FROM Lop
+SELECT * FROM SinhVien
